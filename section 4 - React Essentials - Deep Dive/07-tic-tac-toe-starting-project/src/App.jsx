152ee51afd09688a2987ee563ee41547d8a83842
Juan Cruz <mezzopevajuancruz@gmail.com>
2025-07-19 18:02:58 -0300

    GameBorad component created

--- section 4 - React Essentials - Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ section 4 - React Essentials - Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayers, setActivePlayers] = useState('X');
+
+  function handlePlayerChange() { 
+    setActivePlayers((prevActivePlayer) => (prevActivePlayer === 'X' ? 'O' : 'X'));
+  }
+
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-        <Player name={"Jugador 1"} symbol={"X"}/>
-        <Player name={"Jugador 2"} symbol={"O"}/>           
+        <ol id="players" className="higtlight-player">
+        <Player name={"Jugador 1"} symbol={"X"} isActive={activePlayers === 'X'}/>
+        <Player name={"Jugador 2"} symbol={"O"} isActive={activePlayers === 'O'}/>           
         </ol>
-        GAME BOARD
+        <GameBoard onSelectSquare={handlePlayerChange} activePlayerSymbol={activePlayers}/>
       </div>
     </main>
   );
